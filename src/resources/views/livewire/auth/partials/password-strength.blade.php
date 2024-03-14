7005aa82bac2bfa1933244f4f2989cc40db1a9bd
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2024-03-14 22:44:31 +0800

    added spacing

--- src/resources/views/livewire/auth/partials/password-strength.blade.php
+++ src/resources/views/livewire/auth/partials/password-strength.blade.php
@@ -6,39 +6,39 @@
 
 <div>
     @if($password)
-    <div class="progress">
-        <div
-            @class([
-                'progress-bar',
-                'bg-danger' => $isDanger,
-                'bg-warning' => $isWarning,
-                'bg-success' => $isSuccess,
-            ])
+        <div class="progress">
+            <div
+                @class([
+                    'progress-bar',
+                    'bg-danger' => $isDanger,
+                    'bg-warning' => $isWarning,
+                    'bg-success' => $isSuccess,
+                ])
 
-            @style([
-                'width: 20%' => $score == 0,
-                'width: 40%' => $score == 1,
-                'width: 60%' => $score == 2,
-                'width: 80%' => $score == 3,
-                'width: 100%' => $score == 4,
-            ])
+                @style([
+                    'width: 20%' => $score == 0,
+                    'width: 40%' => $score == 1,
+                    'width: 60%' => $score == 2,
+                    'width: 80%' => $score == 3,
+                    'width: 100%' => $score == 4,
+                ])
 
-            role="progressbar"
-            aria-label="Progress bar"
-            aria-valuenow="{{ $score }}" 
-            aria-valuemin="0" 
-            aria-valuemax="4"
-        ></div>
-    </div>
-    <div class="text-end">
-        <small 
-            @class([
-                'fw-bold',
-                'text-danger' => $isDanger,
-                'text-warning' => $isWarning,
-                'text-success' => $isSuccess,
-            ])
-        >{{ $strength }}</small>
-    </div>
+                role="progressbar"
+                aria-label="Progress bar"
+                aria-valuenow="{{ $score }}" 
+                aria-valuemin="0" 
+                aria-valuemax="4"
+            ></div>
+        </div>
+        <div class="text-end">
+            <small 
+                @class([
+                    'fw-bold',
+                    'text-danger' => $isDanger,
+                    'text-warning' => $isWarning,
+                    'text-success' => $isSuccess,
+                ])
+            >{{ $strength }}</small>
+        </div>
     @endif
 </div>
